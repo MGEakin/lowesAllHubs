346fc229b12bd65f8bc24802ec4839a47635ba19
meakin <meakin@contractor.bbw.com>
2023-06-01 15:35:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# The following libraries have been added for functionality
+# pip install robotframework
+# pip install robotframework-seleniumlibrary
+# pip install robotframework-pabot     ###For parallel testing
+# pip install pysftp
+# pip install minio
+# pip install oic
+# can use import filecmp to compare files
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
